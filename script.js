aba80d8ffc667815ec2077f92366917f0bfc1ef8
Chukwudolue Divine <codevine2023@gmail.com>
2025-02-21 13:33:18 +0100

    responsive check

--- script.js
+++ script.js
@@ -1,11 +1,4 @@
-let inputName = document.getElementById('name');
-let inputEmail = document.getElementById('email');
-let inputMessage = document.getElementById('textarea')
-window.addEventListener('load', () => {
-  inputName.value = "";
-  inputEmail.value = "";
-  inputMessage.value = "";
-});
+
 // TOGGLE THEMES SCRIPT
 const toggleButton = document.getElementById("toggleButton");
 const iconElement = document.getElementById("iconElement");
@@ -68,6 +61,10 @@ toggleButton.addEventListener("click", () => {
   }
 });
 
+
+
+
+
 // Navbar script
 document.addEventListener("DOMContentLoaded", function () {
   var sidebarBtn = document.querySelector(".sidebarBtn");
@@ -84,12 +81,73 @@ document.addEventListener("DOMContentLoaded", function () {
     sidebarBtn.classList.toggle("toggle");
   });
 });
+// nav button animation script
+const contactButton = document.querySelector(".boldContact");
+const circleContact = document.querySelector(".goBack-btn a:nth-child(2)");
 
+
+
+
+
+
+// PROJECT POP UP SCRIPT
+function openDialog(boxNumber) {
+  var dialog = document.getElementById('dialog' + boxNumber);
+  dialog.showModal();
+}
+
+function closeDialog(boxNumber) {
+  var dialog = document.getElementById('dialog' + boxNumber);
+  dialog.close();
+}
+
+
+
+
+
+// Smooth Scroll script
+document.addEventListener("DOMContentLoaded", function () {
+  const scroll = new SmoothScroll('a[href*="#"]', {
+    speed: 500, // Adjust the scroll speed as needed
+    offset: function (anchor, toggle) {
+      // Calculate the offset dynamically based on the screen width
+      if (window.innerWidth < 370) {
+        return 55; // Offset for screens narrower than 370px
+      } else if (window.innerWidth < 500) {
+        return 65; // Offset for screens between 370px and 500px
+      } else if (window.innerWidth < 700) {
+        return 75; // Offset for screens between 500px and 700px
+      } else if (window.innerWidth < 852) {
+        return 85; // Offset for screens between 700px and 852px
+      } else if (window.innerWidth < 992) {
+        return 95; // Offset for screens between 852px and 992px
+      } else {
+        return 0 + "70px"; // Default offset for larger screens
+      }
+    },
+  });
+});
 
 
 
 
 // Contact script
+let inputName = document.getElementById('name');
+let inputEmail = document.getElementById('email');
+let inputMessage = document.getElementById('textarea')
+window.addEventListener('load', () => {
+  inputName.value = "";
+  inputEmail.value = "";
+  inputMessage.value = "";
+});
+
+
+
+
+
+
+
+// Form validation script
 // Use this script to redirect to the custom thank-you page
 
 // document.getElementById('myForm').addEventListener('submit', function (event) {
@@ -116,213 +174,3 @@ document.addEventListener("DOMContentLoaded", function () {
 // });
 
 
-
-
-// PROJECT POP UP SCRIPT
-function openDialog(boxNumber) {
-  var dialog = document.getElementById('dialog' + boxNumber);
-  dialog.showModal();
-}
-
-function closeDialog(boxNumber) {
-  var dialog = document.getElementById('dialog' + boxNumber);
-  dialog.close();
-}
-
-
-// Scroll script
-document.addEventListener("DOMContentLoaded", function () {
-  const scroll = new SmoothScroll('a[href*="#"]', {
-    speed: 500, // Adjust the scroll speed as needed
-    offset: function (anchor, toggle) {
-      // Calculate the offset dynamically based on the screen width
-      if (window.innerWidth < 370) {
-        return 55; // Offset for screens narrower than 370px
-      } else if (window.innerWidth < 500) {
-        return 65; // Offset for screens between 370px and 500px
-      } else if (window.innerWidth < 700) {
-        return 75; // Offset for screens between 500px and 700px
-      } else if (window.innerWidth < 852) {
-        return 85; // Offset for screens between 700px and 852px
-      } else if (window.innerWidth < 992) {
-        return 95; // Offset for screens between 852px and 992px
-      } else {
-        return 0 + "70px"; // Default offset for larger screens
-      }
-    },
-  });
-});
-
-// Animation script
-const contactButton = document.querySelector(".boldContact");
-const circleContact = document.querySelector(".goBack-btn a:nth-child(2)");
-
-// Define the GSAP animation timeline
-const tl = gsap.timeline({ repeat: -1 });
-
-tl.to(contactButton, {
-  duration: 0.1,
-  x: 5,
-  ease: "power1.inOut",
-})
-  .to(contactButton, {
-    duration: 0.1,
-    x: -9,
-    ease: "power1.inOut",
-  })
-  .to(contactButton, {
-    duration: 0.1,
-    x: 9,
-    ease: "power1.inOut",
-  })
-  .to(contactButton, {
-    duration: 0.1,
-    x: -9,
-    ease: "power1.inOut",
-  })
-  .to(contactButton, {
-    duration: 0.1,
-    x: 0,
-    ease: "power1.inOut",
-  })
-  .to({}, { duration: 5 }); // Pause for 5 seconds
-
-// Start the animation
-tl.play();
-
-tl.to(circleContact, {
-  duration: 0.1,
-  x: 5,
-  ease: "power1.inOut",
-})
-  .to(circleContact, {
-    duration: 0.1,
-    x: -9,
-    ease: "power1.inOut",
-  })
-  .to(circleContact, {
-    duration: 0.1,
-    x: 9,
-    ease: "power1.inOut",
-  })
-  .to(circleContact, {
-    duration: 0.1,
-    x: -9,
-    ease: "power1.inOut",
-  })
-  .to(circleContact, {
-    duration: 0.1,
-    x: 0,
-    ease: "power1.inOut",
-  })
-  .to({}, { duration: 5 }); // Pause for 5 seconds
-
-// Start the animation
-tl.play();
-
-const timeLine = gsap.timeline({
-  defaults: {
-    duration: .3
-  }
-});
-
-timeLine
-  .fromTo(
-    ".myNavbar",
-    {
-      y: "-100%",
-    },
-    {
-      y: 0,
-      ease: "bounce",
-    }
-  )
-  .fromTo(
-    ".navbar-brandd p",
-    {
-      opacity: 0,
-    },
-    {
-      opacity: 1,
-    }
-  )
-
-  .fromTo(
-    ".sidebarBtn .span",
-    {
-      x: "-100px",
-    },
-    {
-      x: 0,
-      ease: "bounce",
-      stagger: 0.5,
-    }
-  )
-  .fromTo(
-    ".centerNav a",
-    {
-      y: "-500px",
-    },
-    {
-      y: 0,
-      ease: "power2.in",
-      stagger: 0.1,
-    }
-  )
-  .fromTo(
-    ".display-theme",
-    {
-      scale: 0,
-    },
-    {
-      ease: "bounce",
-      scale: 1,
-    }
-  )
-  .fromTo(
-    ".navBtns a",
-    {
-      scale: 0,
-    },
-    {
-      stagger: 0.2,
-      ease: "bounce",
-      scale: 1,
-    }
-  )
-  .fromTo(
-    " .bottom-page-nav ",
-    {
-      y: "100%",
-    },
-    {
-      y: 0,
-    }
-  )
-  .fromTo(
-    " .bottom-page-nav div",
-    {
-      opacity: 0,
-    },
-    {
-      opacity: 1,
-    }
-  )
-  .fromTo(
-    ".colorPicker-btn",
-    {
-      opacity: 0,
-    },
-    {
-      opacity: 1,
-    }
-  )
-  .fromTo(
-    ".goBack-btn a",
-    {
-      opacity: 0,
-    },
-    {
-      opacity: 1,
-    }
-  );
