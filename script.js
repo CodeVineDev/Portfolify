40f4e686eded898c3c466bf5ba166f60344c83cc
Chukwudolue Divine <codevine2023@gmail.com>
2025-02-22 16:50:35 +0100

    code clean up

--- script.js
+++ script.js
@@ -1,19 +1,16 @@
-
 // TOGGLE THEMES SCRIPT
-const toggleButton = document.getElementById("toggleButton");
-const iconElement = document.getElementById("iconElement");
+const toggleButton = document.getElementById("theme-toggle__btn");
+const iconElement = document.getElementById("theme-toggle__icon");
 
 //COLOR TOOGGLE
 document.addEventListener('DOMContentLoaded', () => {
-  const colorPicker = document.getElementById('colorPicker');
-  const changeColorBtn = document.getElementById('changeColorBtn');
+  const colorPicker = document.getElementById('Picker');
+  const changeColorBtn = document.getElementById('color-picker__btn');
   const root = document.documentElement;
-
   const updateColor = (event) => {
       const hexColor = event.target.value;
       root.style.setProperty('--text-head', hexColor);
   };
-
   changeColorBtn.addEventListener('click', () => {
     colorPicker.click();
   });
@@ -24,21 +21,19 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
 
+
 // Function to enable dark mode
 const enableDarkMode = () => {
   document.body.classList.add("dark-mode");
   localStorage.setItem("dark-mode", "enabled");
 };
-
 // Function to disable dark mode
 const disableDarkMode = () => {
   document.body.classList.remove("dark-mode");
   localStorage.setItem("dark-mode", null);
 };
-
 // Check the current dark mode setting in localStorage
 const darkMode = localStorage.getItem("dark-mode");
-
 // Apply the appropriate theme based on the darkMode setting
 if (darkMode === "enabled") {
   enableDarkMode();
@@ -49,7 +44,6 @@ if (darkMode === "enabled") {
   // Optionally, update the icon to indicate light mode
   iconElement.textContent = "light_mode";
 }
-
 // Add a click event listener to toggle dark mode
 toggleButton.addEventListener("click", () => {
   if (document.body.classList.contains("dark-mode")) {
@@ -67,24 +61,21 @@ toggleButton.addEventListener("click", () => {
 
 // Navbar script
 document.addEventListener("DOMContentLoaded", function () {
-  var sidebarBtn = document.querySelector(".sidebarBtn");
-  var sideBar = document.querySelector(".sideBar");
-  var centerNav = document.querySelector(".centerNav");
-
+  var sidebarBtn = document.querySelector(".navbar__burger-menu");
+  var sideBar = document.querySelector(".side-navbar");
+  var centerNav = document.querySelector(".navbar-links__center");
   centerNav.addEventListener("click", function () {
     sideBar.classList.toggle("active");
     sidebarBtn.classList.toggle("toggle");
   });
-
   sidebarBtn.addEventListener("click", function () {
     sideBar.classList.toggle("active");
     sidebarBtn.classList.toggle("toggle");
   });
 });
 // nav button animation script
-const contactButton = document.querySelector(".boldContact");
-const circleContact = document.querySelector(".goBack-btn a:nth-child(2)");
-
+const contactButton = document.querySelector(".navbar__contact");
+const circleContact = document.querySelector(".back-to a:nth-child(2)");
 
 
 
@@ -95,7 +86,6 @@ function openDialog(boxNumber) {
   var dialog = document.getElementById('dialog' + boxNumber);
   dialog.showModal();
 }
-
 function closeDialog(boxNumber) {
   var dialog = document.getElementById('dialog' + boxNumber);
   dialog.close();
@@ -131,31 +121,34 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
 
+
 // Contact script
 let inputName = document.getElementById('name');
 let inputEmail = document.getElementById('email');
 let inputMessage = document.getElementById('textarea')
 window.addEventListener('load', () => {
+  // window.scrollTo(0, 0); // Scrolls to the top-left corner of the page
   inputName.value = "";
   inputEmail.value = "";
   inputMessage.value = "";
+
+  // Hide the preloader
+  document.getElementById("preloader").style.display = "none";
+
+  // Show the main content
+  document.getElementById("content").style.display = "block";
+
 });
-
-
-
 
 
 
 
 // Form validation script
 // Use this script to redirect to the custom thank-you page
-
 // document.getElementById('myForm').addEventListener('submit', function (event) {
 //   event.preventDefault(); // Prevent default form submission
-
 //   const form = event.target;
 //   const data = new FormData(form);
-
 //   fetch(form.action, {
 //     method: form.method,
 //     body: data,
@@ -174,3 +167,20 @@ window.addEventListener('load', () => {
 // });
 
 
+
+
+
+// THANK YOU PAGE SCRIPT
+// Function to start the confetti animation
+function startConfettiAnimation() {
+  let confetti_container = document.querySelector(".thank-you");
+  let animItem = bodymovin.loadAnimation({
+    wrapper: confetti_container,
+    animType: "svg",
+    loop: false,
+    autoplay: false,
+    path: "https://lottie.host/6dc5f5b5-8697-4e9d-b781-127bc2c459fd/Crhu8Rzi3M.json",
+  });
+  // Play the animation
+  animItem.play();
+}
